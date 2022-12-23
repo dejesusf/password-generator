710ff919ae156a346abf3dacb05436dd5042c1f8
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2022-12-22 21:06:18 -0800

    updates

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 document.getElementById("generate").addEventListener("click", passLength);
 
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-// var passCriteria = ["Length of password? Please choose a number between 8 and 128 characters.", "Would you like to include lowercase letters?", "Would you like to include UPPERCASE letters?", "Would you like to include numbers?", "Would you like to include special characters?"]
-// var passLength
-// var lowerCase = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// var upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-// var specialCase= ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "\\", "|", ":", ";", "'", "<", ",", ".", ">", "?", "/"]
-// var number=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var passCriteria = ["Please choose a number between 8 and 128 for desired character length.", "Would you like to include lowercase letters? Click 'OK' for YES.", "Would you like to include UPPERCASE letters? Click 'OK' for YES.", "Would you like to include numbers? Click 'OK' for YES.", "Would you like to include special characters? Click 'OK' for YES."]
+var length=(i=0, i<8, i++);
+var lowerCase="abcdefghijklmnopqrstuvwxyz" .split('');
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split('');
+var specialCase="~`!@#$%^&*()_-+={}[]|:;<>,.?/" .split('');
+var number="0123456789" .split('');
 
 // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword();
 //   var passwordText = document.querySelector("#password");
-
+  
+//   function 
 //   passwordText.value = password;
-// }
-//popup when nothing is entered or password does not meet critera
-// function passLength () {
-//   prompt(passCriteria[0]);
-//   if (passLength==null){
-//     return;
-//   }
-//   while (passLength <8 || passLength >128){
-//     prompt(passCriteria[0])
-//     if (passLength==null){
-//     return;
-//   }
-// }
 // }
 
 //popups needed for length, lowercase, uppercase, special characters, and numbers
-
-//variables
-var lowerCase= "abcdefghijklmnopqrstuvwxyz";
-var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar= "~`!@#$%^&*()-_+={}[]|\:;'<>,.?/";
-var passLength=0;
-var passCriteria= ["Length of password? Please choose a number between 8 and 128 characters.", "Would you like to include lowercase letters?", "Would you like to include UPPERCASE letters?", "Would you like to include numbers?", "Would you like to include special characters?"]
-
-//function for popups
+function passLength (length) {
+  prompt(passCriteria[0]);
+  if ((length<8) || (length>128)) {
+    return;
+  }
+  else {
+    confirm(passCriteria[1]),
+    lowerCase===true
+  }
+  else {
+    confirm(passCriteria[2]),
+    upperCase===true
+  }
+  else {
+    confirm(passCriteria[3]),
+    number===true
+  }
+  else {
+    confirm(passCriteria[4]),
+    specialCase===true
+  }
+}
